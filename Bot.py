179131171f911aaff585547685dad755b70f5558
golchin0914-hash <golchin0914@gmail.com>
2026-05-17 21:46:21 +0330

    Update and rename B0t.pay to Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,7 @@
+import os
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
 TOKEN = os.environ.get("TOKEN")
 
@@ -8,6 +9,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     name = update.effective_user.first_name
     await update.message.reply_text(f"خوش آمدید {name}! 👋")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
